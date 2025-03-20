99e171e9aff9c81447146e2f8d4efebc8342043b
Aishwarya-rav248 <aishwaryaravinderraj@gmail.com>
2025-03-19 22:30:26 -0500

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ if 'page' not in st.session_state:
 # ----------------------------
 # Login Page
 def login_page():
-    st.markdown("<h1 style='text-align: center; color: #4CAF50;'>Health Score Tracker</h1>", unsafe_allow_html=True)
+    st.markdown("<h1 style='text-align: center; color: #5A5A5A;'>Health Score Tracker</h1>", unsafe_allow_html=True)
     st.title("Login")
     col1, col2 = st.columns(2)
 
